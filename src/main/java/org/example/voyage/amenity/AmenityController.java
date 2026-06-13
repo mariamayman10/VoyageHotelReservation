805ba1b5ecdf470a8087da0c3fbc8448a4f4165a
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-06-13 22:39:55 +0300

    chore: convert controller's base path to plural

--- src/main/java/org/example/voyage/amenity/AmenityController.java
+++ src/main/java/org/example/voyage/amenity/AmenityController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/amenity")
+@RequestMapping("/api/amenities")
 public class AmenityController {
     AmenityService amenityService;
 
