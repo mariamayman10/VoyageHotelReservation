9e56d90eb7abb98e3b59405dfc473b8cd2d84fa7
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-06-13 12:55:38 +0300

    fix: add amenities ignore to handle it manually in service

--- src/main/java/org/example/voyage/hotel/HotelMapper.java
+++ src/main/java/org/example/voyage/hotel/HotelMapper.java
@@ -5,15 +5,17 @@ import org.example.voyage.hotel.dto.CreateHotelRequest;
 import org.example.voyage.hotel.dto.HotelDetailedResponse;
 import org.example.voyage.hotel.dto.UpdateHotelRequest;
 import org.example.voyage.user.UserMapper;
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring", uses = {UserMapper.class, AmenityMapper.class})
 public interface HotelMapper {
+    @Mapping(target = "amenities", ignore = true)
     Hotel toEntity(CreateHotelRequest request);
     HotelDetailedResponse toHotelDetailedResponse(Hotel hotel);
+    List<HotelDetailedResponse> toHotelDetailedResponseList(List<Hotel> hotel);
+    @Mapping(target = "amenities", ignore = true)
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateHotel(UpdateHotelRequest request, @MappingTarget Hotel hotel);
 }
