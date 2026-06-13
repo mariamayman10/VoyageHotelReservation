238b62acfacb8c6f6d652d3855a645e843559858
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-06-13 22:39:21 +0300

    chore: convert controller's base path to plural

--- src/main/java/org/example/voyage/hotel/HotelController.java
+++ src/main/java/org/example/voyage/hotel/HotelController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/hotel")
+@RequestMapping("/api/hotels")
 public class HotelController {
     private final HotelService hotelService;
 
